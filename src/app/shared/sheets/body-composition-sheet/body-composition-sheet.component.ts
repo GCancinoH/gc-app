dfa93c6d3e80f3c46172f188d06f3e9dcea3df32
g.cancinoh <g.cancinoh@gmail.com>
2024-09-05 21:53:45 +0000

    Uploaded

--- src/app/shared/sheets/body-composition-sheet/body-composition-sheet.component.ts
+++ src/app/shared/sheets/body-composition-sheet/body-composition-sheet.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Component, DestroyRef, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatButton } from '@angular/material/button';
+import { MatDivider } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIcon } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'gc-body-composition-sheet',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule, DatePipe,
+    MatInputModule, MatIcon, MatButton, MatFormFieldModule, MatDivider
+  ],
   templateUrl: './body-composition-sheet.component.html',
   styleUrl: './body-composition-sheet.component.css'
 })
 export class BodyCompositionSheetComponent {
+  // injectors
+  fb = inject(FormBuilder);
+  destroyRef = inject(DestroyRef);
+  // observables
+  // signals
+  // inputs, outputs, viewchilds, etc
+  // services
+  // variables
+  todayDate = new Date();
+  bodyCompForm!: FormGroup;
+  // methods
 
+  ngOnInit(): void {
+    this.bodyCompForm = this.fb.group({
+      weight: [''],
+      bmi: [''],
+      visceralFat: [''],
+      muscleMass: [''],
+      bodyFat: ['']
+    });
+  }
+
+  onSubmit(): void {
+    console.log(this.bodyCompForm.value);
+  }
 }
